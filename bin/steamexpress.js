aa588a9d7875436e972798d75e5368c45ce8c81d
Shaun <shaunblacksheep@gmail.com>
2016-06-04 13:31:19 +0000

    Global parameters. Updated version of modules.

--- bin/steamexpress.js
+++ bin/steamexpress.js
@@ -17,38 +17,38 @@ process.exit = exit
 
 // CLI
 
-before(program, 'outputHelp', function () {
+before(program, 'outputHelp', function() {
     this.allowUnknownOption();
 });
 
 program
-.version(version)
-.usage('[options] [dir]')
-.option('-s  --socket', 'add socket.io support')
-.option('    --git', 'add .gitignore')
-.option('-f, --force', 'force on non-empty directory')
-.parse(process.argv);
+    .version(version)
+    .usage('[options] [dir]')
+    .option('-s  --socket', 'add socket.io support')
+    .option('    --git', 'add .gitignore')
+    .option('-f, --force', 'force on non-empty directory')
+    .parse(process.argv);
 
 if (!exit.exited) {
     main();
 }
 
 /**
-* Install a before function; AOP.
-*/
+ * Install a before function; AOP.
+ */
 
 function before(obj, method, fn) {
     var old = obj[method];
 
-    obj[method] = function () {
+    obj[method] = function() {
         fn.call(this);
         old.apply(this, arguments);
     };
 }
 
 /**
-* Prompt for confirmation on STDOUT/STDIN
-*/
+ * Prompt for confirmation on STDOUT/STDIN
+ */
 
 function confirm(msg, callback) {
     var rl = readline.createInterface({
@@ -56,22 +56,23 @@ function confirm(msg, callback) {
         output: process.stdout
     });
 
-    rl.question(msg, function (input) {
+    rl.question(msg, function(input) {
         rl.close();
         callback(/^y|yes|ok|true$/i.test(input));
     });
 }
 
 /**
-* Create application at the given directory `path`.
-*
-* @param {String} path
-*/
+ * Create application at the given directory `path`.
+ *
+ * @param {String} path
+ */
 
 function createApplication(app_name, path) {
     var wait = 3;
 
     console.log();
+
     function complete() {
         if (--wait) return;
         var prompt = launchedFromCmd() ? '>' : '$';
@@ -84,7 +85,8 @@ function createApplication(app_name, path) {
 
         if (launchedFromCmd()) {
             console.log('     %s SET DEBUG=%s:* & npm start', prompt, app_name);
-        } else {
+        }
+        else {
             console.log('     %s DEBUG=%s:* npm start', prompt, app_name);
         }
 
@@ -94,7 +96,8 @@ function createApplication(app_name, path) {
     // socket.io support
     if (program.socket) {
         var app = loadTemplate('js/app-socket.js');
-    } else {
+    }
+    else {
         var app = loadTemplate('js/app.js');
     }
 
@@ -108,13 +111,15 @@ function createApplication(app_name, path) {
         mkdir(path + '/public/images', function() {
             copy_image('images/steam_login.png', path + '/public/images/steam_login.png');
             copy_image('images/steam_logout.png', path + '/public/images/steam_logout.png');
+            copy_image('images/steam_trade_url_button.png', path + '/public/images/steam_trade_url_button.png');
             complete();
         });
 
         mkdir(path + '/views', function() {
             if (program.socket) {
                 copy_template('jade/index-socket.jade', path + '/views/index.jade');
-            } else {
+            }
+            else {
                 copy_template('jade/index.jade', path + '/views/index.jade');
             }
             copy_template('jade/error.jade', path + '/views/error.jade')
@@ -126,25 +131,27 @@ function createApplication(app_name, path) {
 
         // package.json
         var pkg = {
-            name: app_name
-            , version: '0.0.0'
-            , private: true
-            , scripts: { start: 'node app.js' }
-            , dependencies: {
-                'express': '~4.13.1',
-                'body-parser': '~1.13.2',
-                'cookie-parser': '~1.3.5',
+            name: app_name,
+            version: '0.0.0',
+            private: true,
+            scripts: {
+                start: 'node app.js'
+            },
+            dependencies: {
+                'express': '~4.13.4',
+                'body-parser': '~1.15.1',
+                'cookie-parser': '~1.4.3',
                 'debug': '~2.2.0',
                 'morgan': '~1.6.1',
                 'serve-favicon': '~2.3.0',
-                'express-session': '~1.12.1',
-                'openid': '~0.5.13',
+                'express-session': '~1.13.0',
+                'openid': '~2.0.1',
                 'jade': '~1.11.0'
             }
         }
 
         if (program.socket) {
-            pkg.dependencies['socket.io'] = '~1.3.7';
+            pkg.dependencies['socket.io'] = '~1.4.6';
             pkg.dependencies['express-socket.io-session'] = '~1.3.1';
         }
 
@@ -175,22 +182,22 @@ function copy_image(from, to) {
 }
 
 /**
-* Check if the given directory `path` is empty.
-*
-* @param {String} path
-* @param {Function} fn
-*/
+ * Check if the given directory `path` is empty.
+ *
+ * @param {String} path
+ * @param {Function} fn
+ */
 
 function emptyDirectory(path, fn) {
-    fs.readdir(path, function(err, files){
+    fs.readdir(path, function(err, files) {
         if (err && 'ENOENT' != err.code) throw err;
         fn(!files || !files.length);
     });
 }
 
 /**
-* Graceful exit for async STDIO
-*/
+ * Graceful exit for async STDIO
+ */
 
 function exit(code) {
     // flush output for Node.js Windows pipe bug
@@ -205,7 +212,7 @@ function exit(code) {
 
     exit.exited = true;
 
-    streams.forEach(function(stream){
+    streams.forEach(function(stream) {
         // submit empty write request and wait for completion
         draining += 1;
         stream.write('', done);
@@ -215,25 +222,24 @@ function exit(code) {
 }
 
 /**
-* Determine if launched from cmd.exe
-*/
+ * Determine if launched from cmd.exe
+ */
 
 function launchedFromCmd() {
-    return process.platform === 'win32'
-    && process.env._ === undefined;
+    return process.platform === 'win32' && process.env._ === undefined;
 }
 
 /**
-* Load template file.
-*/
+ * Load template file.
+ */
 
 function loadTemplate(name) {
     return fs.readFileSync(path.join(__dirname, '..', 'templates', name), 'utf-8');
 }
 
 /**
-* Main program.
-*/
+ * Main program.
+ */
 
 function main() {
     // Path
@@ -243,15 +249,17 @@ function main() {
     var appName = path.basename(path.resolve(destinationPath));
 
     // Generate application
-    emptyDirectory(destinationPath, function (empty) {
+    emptyDirectory(destinationPath, function(empty) {
         if (empty || program.force) {
             createApplication(appName, destinationPath);
-        } else {
-            confirm('destination is not empty, continue? [y/N] ', function (ok) {
+        }
+        else {
+            confirm('Destination is not empty, continue? [y/N] ', function(ok) {
                 if (ok) {
                     process.stdin.destroy();
                     createApplication(appName, destinationPath);
-                } else {
+                }
+                else {
                     console.error('aborting');
                     exit(1);
                 }
@@ -261,26 +269,28 @@ function main() {
 }
 
 /**
-* echo str > path.
-*
-* @param {String} path
-* @param {String} str
-*/
+ * echo str > path.
+ *
+ * @param {String} path
+ * @param {String} str
+ */
 
 function write(path, str, mode) {
-    fs.writeFileSync(path, str, { mode: mode || 0666 });
+    fs.writeFileSync(path, str, {
+        mode: mode || 0666
+    });
     console.log('   \x1b[36mcreate\x1b[0m : ' + path);
 }
 
 /**
-* Mkdir -p.
-*
-* @param {String} path
-* @param {Function} fn
-*/
+ * Mkdir -p.
+ *
+ * @param {String} path
+ * @param {Function} fn
+ */
 
 function mkdir(path, fn) {
-    fs.mkdirs(path, 0755, function(err){
+    fs.mkdirs(path, 0755, function(err) {
         if (err) throw err;
         console.log('   \033[36mcreate\033[0m : ' + path);
         fn && fn();
